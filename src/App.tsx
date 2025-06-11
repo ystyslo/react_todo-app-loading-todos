e8a99f164dc26b68bbec7139ef91d6cbd37ac3e1
Yura <yurii.st11@gmail.com>
2025-06-11 15:46:42 +0300

    implement suggested changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,22 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useEffect, useState } from 'react';
-import { UserWarning } from './UserWarning';
+
 import * as TodosService from './api/todos';
+import { UserWarning } from './UserWarning';
 import { Todo } from './types/Todo';
 import { TodoList } from './components/TodoList';
-import cn from 'classnames';
 import { Filter } from './types/Filter';
+import { getVisibleTodos } from './utils/getVisibleTodos';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { ErrorMessage } from './components/ErrorMessage';
 
 export const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [filteredTodos, setFilteredTodos] = useState<Todo[]>([]);
-  const [filter, setFilter] = useState<Filter>('all');
+  const [filter, setFilter] = useState<Filter>(Filter.All);
   const [errorMessage, setErrorMessage] = useState('');
   const [title, setTitle] = useState('');
-
-  useEffect(() => {
-    switch (filter) {
-      case 'active':
-        setFilteredTodos(todos.filter(todo => !todo.completed));
-        break;
-      case 'completed':
-        setFilteredTodos(todos.filter(todo => todo.completed));
-        break;
-      default:
-        setFilteredTodos(todos);
-    }
-  }, [todos, filter]);
 
   function showTemporaryError(message: string) {
     setErrorMessage(message);
@@ -36,13 +26,14 @@ export const App: React.FC = () => {
   }
 
   useEffect(() => {
+    setErrorMessage('');
     TodosService.getTodos()
-      .then(loadedTodos => {
-        setFilteredTodos(loadedTodos);
-        setTodos(loadedTodos);
-      })
+      .then(setTodos)
       .catch(() => showTemporaryError('Unable to load todos'));
   }, []);
+
+  const visibleTodos = getVisibleTodos(todos, filter);
+  const isAllTodosCompleted = todos.every(todo => todo.completed);
 
   function addTodo(newTodo: Todo) {
     setTodos(prevTodos => [...prevTodos, newTodo]);
@@ -81,103 +72,29 @@ export const App: React.FC = () => {
       <h1 className="todoapp__title">todos</h1>
 
       <div className="todoapp__content">
-        <header className="todoapp__header">
-          {/* this button should have `active` class only if all todos are completed */}
-          <button
-            type="button"
-            className={cn('todoapp__toggle-all', {
-              active: todos.every(todo => todo.completed === true),
-            })}
-            data-cy="ToggleAllButton"
-          />
-
-          {/* Add a todo on form submit */}
-          <form onSubmit={handleSubmit}>
-            <input
-              data-cy="NewTodoField"
-              type="text"
-              className="todoapp__new-todo"
-              placeholder="What needs to be done?"
-              value={title}
-              onChange={handleTitle}
-            />
-          </form>
-        </header>
-
-        <TodoList todos={filteredTodos} onTodoDelete={deleteTodo} />
-
-        {/* Hide the footer if there are no todos */}
-        {!!todos.length && (
-          <footer className="todoapp__footer" data-cy="Footer">
-            <span className="todo-count" data-cy="TodosCounter">
-              {`${todos.filter(todo => !todo.completed).length} items left`}
-            </span>
-
-            {/* Active link should have the 'selected' class */}
-            <nav className="filter" data-cy="Filter">
-              <a
-                href="#/"
-                className={cn('filter__link', { selected: filter === 'all' })}
-                data-cy="FilterLinkAll"
-                onClick={() => setFilter('all')}
-              >
-                All
-              </a>
-
-              <a
-                href="#/active"
-                className={cn('filter__link', {
-                  selected: filter === 'active',
-                })}
-                data-cy="FilterLinkActive"
-                onClick={() => setFilter('active')}
-              >
-                Active
-              </a>
-
-              <a
-                href="#/completed"
-                className={cn('filter__link', {
-                  selected: filter === 'completed',
-                })}
-                data-cy="FilterLinkCompleted"
-                onClick={() => setFilter('completed')}
-              >
-                Completed
-              </a>
-            </nav>
-
-            {/* this button should be disabled if there are no completed todos */}
-            <button
-              type="button"
-              className="todoapp__clear-completed"
-              data-cy="ClearCompletedButton"
-              disabled={todos.every(todo => todo.completed === false)}
-            >
-              Clear completed
-            </button>
-          </footer>
-        )}
-      </div>
-
-      {/* DON'T use conditional rendering to hide the notification */}
-      {/* Add the 'hidden' class to hide the message smoothly */}
-      <div
-        data-cy="ErrorNotification"
-        className={cn(
-          'notification is-danger is-light has-text-weight-normal',
-          { hidden: !errorMessage },
-        )}
-      >
-        <button
-          data-cy="HideErrorButton"
-          type="button"
-          className="delete"
-          onClick={() => setErrorMessage('')}
+        <Header
+          title={title}
+          onTitleChange={handleTitle}
+          onSubmit={handleSubmit}
+          isAllTodosCompleted={isAllTodosCompleted}
         />
-        {/* show only one message at a time */}
-        {errorMessage}
+
+        <TodoList todos={visibleTodos} onTodoDelete={deleteTodo} />
+
+        {!!todos.length && (
+          <Footer
+            todos={todos}
+            filter={filter}
+            onFilterChange={setFilter}
+            isAllTodosCompleted={isAllTodosCompleted}
+          />
+        )}
       </div>
+
+      <ErrorMessage
+        errorMessage={errorMessage}
+        onErrorClose={setErrorMessage}
+      />
     </div>
   );
 };
--- src/types/Filter.ts
+++ src/types/Filter.ts
@@ -1,1 +1,5 @@
-export type Filter = 'all' | 'completed' | 'active';
+export enum Filter {
+  All = 'all',
+  Active = 'active',
+  Completed = 'completed',
+}
